4b1deaab0b3f1cd24601d0999aa1f0b68a60969f
Omar El-Domeiri <doesnotexist@users.noreply.github.com>
2021-10-04 10:29:44 -0400

    add login / logout button

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,7 +1,10 @@
+import { useAuth } from '@redwoodjs/auth'
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const HomePage = () => {
+  const { logIn, logOut, isAuthenticated } = useAuth()
+
   return (
     <>
       <MetaTags
@@ -10,14 +13,11 @@ const HomePage = () => {
         /* you should un-comment description and add a unique description, 155 characters or less
       You can look at this documentation for best practices : https://developers.google.com/search/docs/advanced/appearance/good-titles-snippets */
       />
-
       <h1>HomePage</h1>
       <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
+        <button onClick={isAuthenticated ? logOut : logIn}>
+          {isAuthenticated ? 'log out' : 'log in'}
+        </button>
       </p>
     </>
   )
